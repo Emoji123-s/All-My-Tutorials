06c4eb34111031b47d3a34056702143d70ec60d8
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2024-02-12 17:08:50 +0100

    Python User Inputs

--- Python/Course Practicals/strmanipulation.py
+++ Python/Course Practicals/strmanipulation.py
@@ -1,9 +1,13 @@
+#A program to learn string Concatenation
+
 print("hello world\nhello world\nhello world")
 
 print("hello" + "divine")
-print("hello " + "divine")
-print("hello" + " divine")
-print("hello" + " " + "divine")
+
+#To concatenate a string, there are 3 ways
+print("hello " + "divine") # Once printed, there will be no space between them
+print("hello" + " divine") # Once printed, there will be a spce, because we added a space to the "divine" string before printing. You can also add a space after the "o" in "hello" to get the same result
+print("hello" + " " + "divine") # Once printed, a space would be added between the strings "hello" and "divine", because of the blank space character.
 
 print("String Manipulation Exercise")
 print('String Concatenation is done with "+" sign')
---
+++ Python/Course Practicals/user_inputs.py
@@ -0,0 +1,3 @@
+input("what is your name?")
+
+print(input("What is your name?\n"))
