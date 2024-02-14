9900222152655fd2c43da5c4255293cf5cb41430
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2024-02-14 23:13:34 +0100

    Added Day25-27

---
+++ Python/Course Practicals/variables.py
@@ -0,0 +1,13 @@
+# Program to ask for user's name, and then print the length of that name
+
+name = (input('What is your name: '))
+print(name)
+# To print the length of the given name
+print(len(name))
+print(type(name))
+
+#OR
+#name=(input("what is your name: "))
+#print(name)
+#length=(len(name))
+#print(length)
