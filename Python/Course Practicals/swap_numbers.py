2b2b678cae4a797cd4f34de42532eab8581c6f5b
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2024-02-17 16:20:21 +0100

    Simple Program to swap 2 numbers

---
+++ Python/Course Practicals/swap_numbers.py
@@ -0,0 +1,19 @@
+# program to swap 2 numbers
+
+print("Welcome to your Number Swapper :)")
+print("Input your selected numbers below")
+# First, let's take the input from the user
+
+a = input("Enter your number for a: ")
+b = input("Enter your number for b: ")
+
+# To swap, we would need a third container/variable, and our input should be a=value of b, and b= value of a
+#Let's try adding a third variable
+# since a=value of b, and b=value of a
+c=b
+b=a
+
+print("a =" + " " + c)
+print("b =" + " " + b)
+
+print("Cool, innit? :D")
