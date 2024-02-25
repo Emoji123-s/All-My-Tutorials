999099b0e46bed2107aa71ab07d6761d7dd511cc
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2024-02-25 15:18:10 +0100

    Python Operators

--- Python/Course Practicals/operators.py
+++ Python/Course Practicals/operators.py
@@ -68,3 +68,56 @@ print(c)
 a = 5
 print(a < 5)
 print(a == 5)
+
+
+# Logical Operators
+# The following are logical operators
+"""
+and - If both of the statements are true, then and prints true. If one of the statements is true, and prints false
+or - If one of the statements is true, then the result is true
+not - It reverses the result of a given statement. So if a statement is true, not prints false, and vice versa
+"""
+
+# For and
+a,b = 5,4
+print(a>4 and b<10)
+print(a>4 and b>10)
+
+# For or
+print(a>4 or b<10)
+print(a>4 or b>10)
+
+# For not
+print(a>4)
+print(not(a))
+
+
+# Bitwise Operators
+# The following are bitwise operators
+"""
+& - bitwise and - If both value is 1, the result is 1
+| - bitwise or - If a value is 1, the result is one
+^ - bitwise xor - If both values are 1, the result is zero
+~ - not / complement - reverses the bit into a negative value, i.e, ~5 = -6. We can't store a negative answer in menory, so we use 2's complement, and the formula is
+                    2's = 1's + 1
+                    where 1's = reversing of the positive bits of the not result
+<< - left shift - This operator gains bit by shifting the bits of a number to the left. The simple way is this
+                    x * 2^n
+                    where x = number to be shifted
+                          n = bits to be added
+>> - right shift - This operator loses bit by shifting the bits of a number to the right. The simple way is this
+                    x / 2^n
+                    where x = number to be shifted
+                          n = bits to be added
+
+The bitwise operators as the name suggests works on bits. So if we have
+""" 
+c,d = 6,7
+
+print(c & d) # First the numbers get converted to their binary form, stored in either a 4 or 8-bit format, then bitwise and is performed
+print(c | d)
+print(~c)
+print(~b)
+print(c ^ d)
+print(c >> d)
+print(c << d) 
