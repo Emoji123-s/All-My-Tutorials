0832d521e6639ff6c6ffc75b8a96d32ddbccde89
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2024-03-13 15:05:30 +0100

    Nested If-else Elif and Glo USSD Service Mimicry

---
+++ Python/Course Practicals/nested_if_else_elif.py
@@ -0,0 +1,88 @@
+"""
+Nested If
+It is basically an if statement inside an if statement
+
+if condition1:
+    statement1
+    if condition2:
+        statement2
+
+Any number of nested if is possible. The indentation in python is key to identufy the level of nesting
+
+Nested If-Else
+It is considered if a complete if-else construct is within an if or an else block.
+
+if condition1:
+    statement1
+    if condition2:
+        statement2
+    else:
+        statement3
+else:
+    statement4 - It can also be situated here
+
+
+# Nested If-Else - Rollercoaster Ride
+# If you are more than or equal to 3 feet, you can ride, and if your age is less than or equal to 18, you will pay 250 naira, and if it is  greater than 18, you'll pay 500 naira
+
+# Age from the user, and probably the name for future documentation
+name = input("Enter your Name : ")
+print(f"Hi {name}, fill out the following")
+
+height = int(input("Enter your height in feet : "))
+
+if (height >= 3):
+    print("You can ride")
+    age = int(input("How old are you ? "))
+    if (age <= 18):
+        print("Pay 250 naira")
+    else:
+        print("Pay 500 naira")
+else:
+    print("Too short for this ride")
+print("Bye!")
+"""
+
+"""
+Elif
+
+It means else-if, and it is used for multiple case statements
+Supposedly we wanted to add if age > 12 for 150, then if age is between 12-18, then greater than 18 is 500
+
+
+name = input("Enter your Name : ")
+print(f"Hi {name}, fill out the following")
+
+height = int(input("Enter your height in feet : "))
+
+if (height >= 3):
+    print("You can ride")
+    age = int(input("How old are you ? "))
+    if (age <= 12):
+        print("Pay 1500 naira")
+    elif (age <= 18):
+        print("Pay 2500 naira")
+    elif (age > 18):
+        print("Pay 5000 naira")
+else:
+    print("Too short for this ride")
+"""
+
+"""
+If a user enters 1, it should print One, if 2, it should print 2, if 3, should print 3, Like a number familiarizer
+"""
+print("Number Familiarizer")
+# Take number from the user
+number = int(input("Enter a number : "))
+
+# Conditions
+if (number == 1):
+    print(f"{number} - This is number One")
+elif (number == 2):
+    print(f"{number} - This is number Two")
+elif (number == 3):
+    print(f"{number} - This is number Three")
+elif (number == 4):
+    print(f"{number} - This is number Four")
+else:
+    print("Wrong Input")
---
+++ Python/Projects/ussd_service.py
@@ -0,0 +1,159 @@
+# A program to mimic Glo's Ussd service. Assuming the user already dialed *312#
+first_format = """
+1 = Buy Data
+2 = Gift Data
+3 = Share Data
+4 = Check Data Bal
+5 = Voice / Data Roaming Offers
+6 = Glo Reward - CashToken
+0 = Exit
+"""
+print(first_format)
+number = int(input("Enter a number : "))
+
+if (number == 1):
+    format = """
+    1 = Proceed (Auto-renew)
+    2 = Proceed (One-off)
+    99 = Back
+    0 = Exit
+    """
+    print(format)
+
+    inside = int(input("Enter a number : "))
+    if (inside == 1):
+        format1 = """
+    1 = Mini Plans
+    2 = Monthly Plans
+    3 = Mega Plans
+    4 = Super Mega Plans
+    5 = Special Data Offer
+    6 = Social Bundles
+    7 = Night and Weekend Plans
+    8 = GLOTV Plans 
+    """
+        print(format1)
+        inside1 = int(input("Enter a number : "))
+        if (inside1 == 1):
+            format_1 = """
+            1 = N100 = 150MB 1 Day incl 35MB nite
+            2 = N200 = 350MB 2 Day incl 110MB nite
+            3 = N500 = 1.8GB 14 Days incl 1GB nite
+            4 = N50 = 50MB 1 Day incl 5MB nite
+            """
+            print(format_1)
+
+            inside_2 = int(input("Enter a number : "))
+            if (inside_2 == 1):
+                print(f"Activation of 150MB 1 Day incl 35MB nite is successful and will expire tomorrow. See messages for full details")
+
+
+    elif (inside == 2):
+        format2 = """
+    1 = Mini Plans
+    2 = Monthly Plans
+    3 = Mega Plans
+    4 = Super Mega Plans
+    5 = Special Data Offer
+    6 = Social Bundles
+    7 = Night and Weekend Plans
+    8 = GLOTV Plans 
+    """
+        print(format2)
+        inside1 = int(input("Enter a number : "))
+        if (inside1 == 1):
+            format_1 = """
+            1 = N100 = 150MB 1 Day incl 35MB nite
+            2 = N200 = 350MB 2 Day incl 110MB nite
+            3 = N500 = 1.8GB 14 Days incl 1GB nite
+            4 = N50 = 50MB 1 Day incl 5MB nite
+            """
+            print(format_1)
+            inside_2 = int(input("Enter a number : "))
+            if (inside_2 == 1):
+                print(f"Activation of 150MB 1 Day incl 35MB nite is successful and will expire tomorrow. See messages for full details")
+
+    elif (inside == 99):
+        format3 = """
+        1 = Buy Data
+        2 = Gift Data
+        3 = Share Data
+        4 = Check Data Bal
+        5 = Voice / Data Bal
+        6 = Glo Reward - CashToken
+        0 = Exit
+    """
+        print(format3)
+    elif (inside == 0):
+        print("Glo, Rule your World")
+    else:
+        print("Not recognized, try again")
+
+elif (number == 2):
+    format1 = """
+    1 = Mini Plans
+    2 = Monthly Plans
+    3 = Mega Plans
+    4 = Super Mega Plans
+    5 = Special Data Offer
+    6 = Social Bundles
+    7 = Night and Weekend Plans
+    8 = GLOTV Plans 
+    """
+    print(format1)
+    inside1 = int(input("Enter a number : "))
+    if (inside1 == 1):
+            format_1 = """
+            1 = N100 = 150MB 1 Day incl 35MB nite
+            2 = N200 = 350MB 2 Day incl 110MB nite
+            3 = N500 = 1.8GB 14 Days incl 1GB nite
+            4 = N50 = 50MB 1 Day incl 5MB nite
+            """
+            print(format_1)
+            inside_2 = int(input("Enter a number : "))
+            if (inside_2 == 1):
+                usr_number = int(input("Enter a number to gift: "))
+                print(f"Please press 0 to confirm if you want to gift {usr_number} N100 = 150MB 1 Day incl 35MB")
+                container = int(input())
+                print("Sorry, You are not viable to use this feature")
+
+elif (number == 3):
+    format1 = """
+    1 = Share Data
+    2 = Unshare Data
+    3 = List of Shared Data
+    4 = Get Data Settings
+    5 = Manual Configuration Settings
+    6 = Easy Share (Credit Share)
+    0 = Exit 
+    """
+    print(format1)
+
+elif (number == 4):
+    print("Dear Customer, your plan has expired and you do not have a data plan. To buy a data plan and continue browsing, visit https://hsi.glo.com or dial *312#.")
+
+elif (number == 5):
+    format1 = """
+    1 = My Tariff Plan
+    2 = Int'l Call Offers
+    3 = Data Roaming Offers
+    4 = Berekete 10x
+    5 = Glo 10Kobo
+    6 = Always On
+    99 = Back
+    0 = Exit 
+    """
+    print(format1)
+
+elif (number == 6):
+    format1 = """
+    Recharge and Win assured cashback, exciting prizes and a chance to win 100M every week
+    1 = Opt-in
+    2 = Opt-out
+    99 = Back
+    0 = Exit 
+    """
+    print(format1)
+
+else:
+    print("USSD must be 1-150 characters of length. Please try again")
