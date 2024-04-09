d07c379cd6b4a08f35705c9c4dd266645c855b71
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2024-04-09 19:05:19 +0100

    Password Generator

--- Python/Projects/password_generator.py
+++ Python/Projects/password_generator.py
@@ -1,21 +1,45 @@
 """
-Develop a password generator that asks the user for the desired length of the password and the type of characters to include (e.g, letters, numbers, symbols)
-Concatenate randomly selected characters to generate the password
-Print the generated password using the print function
+A Password Generator. Take input from the user, for amount of letters, amount of symbols, amount of numbers. From the user input, generate a password. There are 2 levels, the easy where the password isn't shuffled from the user input, and the hard level where the password is shuffled from the user input.
+
+Easy
+Generate random numbers and symbols. The password should take the amount of numbers, symbols and letters and form a password without shuffling. Exactly in the same order that was provided.
+
+Hard
+Generate random numbers and symbols. The password should take the amount of numbers, symbols and letters and form a password. This time, the password should be shuffled every time the program runs
 """
 
-import random
+# For Easy
+
 # Prompt Message
-print("Paswword Generator")
+print("Password Generator")
+
+# Importing Library
+import random
 
 # User Input
-length = int(input("How long do you want the password to be? "))
-character_type = input("Type of characters to be included (Numbers, Letters, Symbols), seperated by a comma: ")
+amount_of_letters = int(input("How many letters do you want in your password? "))
+amount_of_symbols = int(input("How many symbols do you want in your password? "))
+amount_of_numbers = int(input("How many numbers do you want in your password? "))
 
-# Splitting the String 
-character_type_split = character_type.split(",")
+# Character Pool holding the uppercase, lowercase, symbols and numbers
+uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+special_characters = "!@#$%^&*()_+-=[]{}|;:,.<>?`~"
+numbers = '0123456789'
 
-# Random
-character_random= random.choice(character_type_split)
-character_sample = random.sample(character_type_split, length)
-character_print = print(f"{character_sample}")
+letters = uppercase_letters + lowercase_letters
+
+# Using the random.choice module
+
+password = ""
+for i in range(amount_of_letters):
+    password += random.choice(letters)
+
+for i in range(amount_of_symbols):
+    password += random.choice(special_characters)
+
+for i in range(amount_of_numbers):
+    password += random.choice(numbers)
+
+# Printing the Password
+print(password)
