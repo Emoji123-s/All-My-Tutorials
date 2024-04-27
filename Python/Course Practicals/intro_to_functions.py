47bf63f31bfff85ce716abc7fb311f300be544a6
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2024-04-27 19:02:01 +0100

    added withdrawal function

---
+++ Python/Course Practicals/intro_to_functions.py
@@ -0,0 +1,33 @@
+"""
+A function is more like a container for holding code blocks to be repeated in a program, rather than writing that same code over and over. It aids time management and flexibility when working on programs, as a function can be called in different places across a program. To create a function:
+1.  Define the function
+2.  Call the function
+
+1.  Define a function:
+Syntax:
+            def function_name(parameters):
+            statement(s)
+            return statement(s)
+    It is optional for the function to have parameters, and the return expression is also optional.
+
+2.  Call a function
+Syntax:
+            function_name(parameters)
+
+Types of Function
+1.    Built-in Functions
+2.    User-Defined Functions
+
+1.  Built-in Functions
+Built-in functions are functions that are already defined in the Python language. They can be used without having to define them first. They can be used in any program. Examples are print(), len(), max(), min(), type()
+
+2.  User-Defined Functions
+They are functions defined by users based on the program's need.
+"""
+# Function Definition
+def greet():
+    print("Hi")
+    print("Divine Providence")
+
+# Function Call
+greet()
---
+++ Python/Projects/atm_withdrawal_service.py
@@ -0,0 +1,146 @@
+# Import library
+import time 
+from pin import pin
+
+# Assuming the user has slotted in their card. Display Message Prompt
+print("ATM Withdrawal.")
+time.sleep(2)
+print("Insert your card")
+time.sleep(2)
+
+# Display "Please Wait" with a wait time
+print("Please Wait....")
+
+# Using the time import to wait for 4 seconds to mimic the loading time of the machine to process
+time.sleep(4)
+
+# Using while loop to loop the program until the user selects "no" at the end of the transaction
+while True:
+    # Request pin
+    user_pin = (input("Enter your PIN : "))
+
+    # Condition
+    if (user_pin == pin):
+        time.sleep(2)
+        print("Select your Service from the list")
+        print("1. Recharge")
+        print("2. Balance Enquiry")
+        print("3. Cash Withdrawal")
+        print("4. Cash Deposit")
+        print("5. Change PIN")
+        print("6. Exit")
+
+        # User input fot withdrawal
+        user_choice = int(input())
+
+        # Condition for withdrawal
+        time.sleep(1)
+        if (user_choice == 3):
+            print("1. Current")
+            print("2. Savings")
+            print("3. Joint")
+            print("4. Zero")
+            # If user choice is savings
+            user_choice = int(input())
+            if (user_choice == 2):
+                time.sleep(1)
+                print("Do you want a receipt for this transaction? ")
+                print("1. Yes")
+                print("2. No")
+                user_choice = int(input())
+                if (user_choice == 1):
+                    print("Sorry, Receipts are not available at this time")
+                    time.sleep(1)
+                    print("Enter an amount")
+                    print("1. 1000")
+                    print("2. 2000")
+                    print("3. 3000")
+                    print("4. 4000")
+                    print("5. 5000")
+                    print("6. 10000")
+                    print("7. 20000")
+                    print("8. Other")
+                    user_choice = int(input())
+                    if (user_choice == 6):
+                        print("Cash is Counted")
+                        print("Cash Retract is disabled")
+                        time.sleep(5)
+                        print("Please take your cash")
+                        time.sleep(3)
+                        print("Do you want to perform another transaction? ")
+                        print("1. Yes")
+                        print("2. No")
+                        user_choice = int(input())
+                        if (user_choice == 1):
+                            continue
+                        else:
+                            print("Thank you for using our service!")
+                            break
+                    elif (user_choice == 8):
+                        other_amount = int(input("Enter an amount : "))
+                        time.sleep(1)
+                        print("Cash is Counted")
+                        time.sleep(1)
+                        print("Cash Retract is disabled")
+                        time.sleep(5)
+                        print("Please take your cash")
+                        time.sleep(3)
+                        print("Do you want to perform another transaction? ")
+                        print("1. Yes")
+                        print("2. No")
+                        user_choice = int(input())
+                        if (user_choice == 1):
+                            continue
+                        else:
+                            print("Thank you for using our service!")
+                            break
+                if (user_choice == 2):
+                    time.sleep(1)
+                    print("Enter an amount")
+                    print("1. 1000")
+                    print("2. 2000")
+                    print("3. 3000")
+                    print("4. 4000")
+                    print("5. 5000")
+                    print("6. 10000")
+                    print("7. 20000")
+                    print("8. Other")
+                    user_choice = int(input())
+                    if (user_choice == 6):
+                        print("Cash is Counted")
+                        time.sleep(1)
+                        print("Cash Retract is disabled")
+                        time.sleep(5)
+                        print("Please take your cash")
+                        time.sleep(3)
+                        print("Do you want to perform another transaction? ")
+                        print("1. Yes")
+                        print("2. No")
+                        user_choice = int(input())
+                        if (user_choice == 1):
+                            continue
+                        else:
+                            print("Thank you for using our service!")
+                            break
+                    elif (user_choice == 8):
+                        other_amount = int(input("Enter an amount : "))
+                        time.sleep(1)
+                        print("Cash is Counted")
+                        time.sleep(1)
+                        print("Cash Retract is disabled")
+                        time.sleep(5)
+                        print("Please take your cash")
+                        time.sleep(3)
+                        print("Do you want to perform another transaction? ")
+                        print("1. Yes")
+                        print("2. No")
+                        user_choice = int(input())
+                        if (user_choice == 1):
+                            continue
+                        else:
+                            print("Thank you for using our service!")
+                            break
+
+
+else:
+    print("Access Denied")
