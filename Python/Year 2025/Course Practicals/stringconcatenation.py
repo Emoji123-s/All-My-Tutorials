2f0692813c264c8fddd4e0b259c01ce144b89388
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2025-09-10 10:52:50 +0100

    string concatenation

---
+++ Python/Year 2025/Course Practicals/stringconcatenation.py
@@ -0,0 +1,8 @@
+#A program to learn string concatenation
+
+print("Hello World\nHello World\nHello World")
+
+#to concatenate strings we use the + operator
+print("Hello" + "World") #adding no space
+print("Hello" + " " + "World") #adding space
+print("Hello" + " World") #adding space
