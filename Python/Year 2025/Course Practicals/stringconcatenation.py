93613a2a2ca4158cc643d06e08709aecc21d1dde
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2025-09-10 14:09:41 +0100

    Update stringconcatenation

--- Python/Year 2025/Course Practicals/stringconcatenation.py
+++ Python/Year 2025/Course Practicals/stringconcatenation.py
@@ -6,3 +6,17 @@ print("Hello World\nHello World\nHello World")
 print("Hello" + "World") #adding no space
 print("Hello" + " " + "World") #adding space
 print("Hello" + " World") #adding space
+
+#we can also use the , operator to concatenate strings
+print("Hello", "World") #adds space by default
+print("Hello", " ", "World") #adds space by default
+print("Hello", " World") #adds space by default
+print("Hello" + ", " + "World") #adding comma and space
+
+# Exercise
+# Concatenate the following strings to form a sentence and print it
+# Using the first method
+print("String Manipulation Exercise\nString Concatenation is done with the + operator\neg, print('Hello' + 'World')\nNew lines can be created with a backslash and n.")
+
+# Using the second method
+print("String Manipulation Exercise" + "String Concatenation is done with the + operator" + "eg, print('Hello' + 'World')" + "New lines can be created with a backslash and n.")
