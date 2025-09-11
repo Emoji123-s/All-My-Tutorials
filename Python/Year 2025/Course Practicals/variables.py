5f4b2d2be4dd4d594317ca90544c95fa30350668
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2025-09-11 13:43:18 +0100

    Create variables.py

---
+++ Python/Year 2025/Course Practicals/variables.py
@@ -0,0 +1,6 @@
+# Variables
+
+# Program to print the length of user input string
+name = input("Enter your name: ") # taking input from the user
+print("Hello", name) # printing the input
+print("Length of your name is:", len(name)) # printing the length of the input string
