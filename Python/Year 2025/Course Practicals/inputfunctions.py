649f2615f6e8ee5166b94357c38d1efcbcded277
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2025-09-11 13:24:08 +0100

    Create inputfunctions.py

---
+++ Python/Year 2025/Course Practicals/inputfunctions.py
@@ -0,0 +1,15 @@
+# Input Functions
+# The input function is used to take input from the user in Python.
+# It reads a line from input, converts it into a string (stripping a trailing newline), and returns that.
+# Syntax: input(prompt)
+# Example 1: Taking a simple input
+name = input("Enter your name: ")
+print(name)
+
+# By default, the input function takes input as a string, and returns it as a string.
+# Example 2: Taking an integer input
+age = int(input("Enter your age: ")) # converting the input string to an integer
+print(age)
+
+print("Hello" + " " + name )
+print("Hello", input("Enter your name: ")) # taking input directly in the print function
