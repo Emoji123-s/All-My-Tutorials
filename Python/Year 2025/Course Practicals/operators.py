87ba9371ce0f59d5a5debfeffc37758d858c5cfc
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2025-09-19 13:49:16 +0100

    Update operators.py

--- Python/Year 2025/Course Practicals/operators.py
+++ Python/Year 2025/Course Practicals/operators.py
@@ -15,10 +15,10 @@ Aritmetic Operators
 % = Modulus/Remainder
 // = Flow Division (changes from float to integer)
 ** = Power
-"""
 
-#PEMDAS
-# Parenthesis, Exponents, Multiplication/Division, Addition/Subtraction. 
+
+PEMDAS
+- Parenthesis, Exponents, Multiplication/Division, Addition/Subtraction. 
 print(5 + 2) # Addition
 print(5 - 2) # Subtraction
 print(5 * 2) # Multiplication
@@ -29,4 +29,44 @@ print(5 ** 2) # Exponentiation
 print(5 + 2 * 3) # Multiplication first, then addition
 print((5 + 2) * 3) # Parenthesis first, then multiplication
 
+shorthand assignment operators include:
++=
+-=
+*=
+/=
+%=
+//=
+**=
+For example:
+a = 5
+a += 2 # This is the same as a = a + 2
+print(a) # This will print 7
+You can also do more than one assignment at a time:
+a,b = 5,4
+print(a) # This will print 5
+print(b) # This will print 4
 
+"""
+
+"""
+Comparison Operators
+== Equal to
+!= Not equal to
+> Greater than
+< Less than
+>= Greater than or equal to
+<= Less than or equal to
+
+In comparison operators, there must be a container to compare, for example:
+print(5 == 2) # Equal to
+print(5 != 2) # Not equal to
+print(5 > 2) # Greater than
+print(5 < 2) # Less than
+print(5 >= 2) # Greater than or equal to
+print(5 <= 2) # Less than or equal to
+
+Doing 5 == a will give an error, because there is no container to compare 5 to.
+
+It will return a boolean value, either True or False, depending on the comparison.
+
+"""
