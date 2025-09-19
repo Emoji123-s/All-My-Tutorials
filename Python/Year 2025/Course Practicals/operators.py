06fb2f7db789e52006ea091c6ee9cc4b928a6468
Isaac-Victor Providence <isaacvictorprovidence@gmail.com>
2025-09-19 12:48:25 +0100

    Create operators.py

---
+++ Python/Year 2025/Course Practicals/operators.py
@@ -0,0 +1,32 @@
+# Operators
+
+"""
+They are simply symbols or operators that are used to perform functions in Python. For example
+
+                                        5 + 2
+                                5,2 = Operands
+                                + = Operator
+
+Aritmetic Operators
++ = Plus/Addition
+- = Minus/Subtraction
+* = Times/Multiplication
+/ = Division (Outputs in float)
+% = Modulus/Remainder
+// = Flow Division (changes from float to integer)
+** = Power
+"""
+
+#PEMDAS
+# Parenthesis, Exponents, Multiplication/Division, Addition/Subtraction. 
+print(5 + 2) # Addition
+print(5 - 2) # Subtraction
+print(5 * 2) # Multiplication
+print(5 / 2) # Division
+print(5 % 2) # Modulus, prints the remainder, in thsis case, 1, because 2 goes into 5 twice, with 1 remaining.
+print(5 // 2) # Floor Division
+print(5 ** 2) # Exponentiation
+print(5 + 2 * 3) # Multiplication first, then addition
+print((5 + 2) * 3) # Parenthesis first, then multiplication
+
+
